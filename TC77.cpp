95fc6bbb3148a4dfe4e60a4dc75c4800d20e0885
chisl <info@chisl.io>
2017-12-27 18:16:16 +0100

    Initial commit

---
+++ TC77.cpp
@@ -0,0 +1,13 @@
+/*
+ * name:        TC77
+ * description: Serially accessible digital temperature sensor particularly suited for low cost and small form-factor applications.
+ * manuf:       Microchip
+ * version:     0.1
+ * url:         http://ww1.microchip.com/downloads/en/DeviceDoc/20092B.pdf
+ * date:        2016-08-17
+ * author       https://chisl.io/
+ * file:        TC77.cpp
+ */
+
+#include "TC77.hpp"
+
